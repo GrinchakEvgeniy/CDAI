261220773bb3b35c6426b03f67792a20da6ef9dc
GrinchakEvgeniy <evgeniy.smartpipl@gmail.com>
2021-01-30 14:24:57 +0200

    fin

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,4 @@
-function printPDF() {
-    html2canvas(document.body).then(function (canvas) {
-    var img = canvas.toDataURL("image/png");
-    var doc = new jsPDF();
-    doc.addImage(img, 'JPEG', 1, 1);
-    doc.save('test.pdf');
-});
-}
-
 function getPDF(){
-
 		var HTML_Width = $("#pdf").width();
 		var HTML_Height = $("#pdf").height();
 		var top_left_margin = 15;
@@ -16,31 +6,23 @@ function getPDF(){
 		var PDF_Height = (PDF_Width*1.5)+(top_left_margin*2);
 		var canvas_image_width = HTML_Width;
 		var canvas_image_height = HTML_Height;
-
 		var totalPDFPages = Math.ceil(HTML_Height/PDF_Height)-1;
-
-
 		html2canvas($("#pdf")[0],{
 			allowTaint:true,
 			useCORS : true,
 		}).then(function(canvas) {
 			canvas.getContext('2d');
-
-			console.log(canvas.height+"  "+canvas.width);
-
-
 			var imgData = canvas.toDataURL("image/jpeg", 1.0);
 			var pdf = new jsPDF('p', 'pt',  [PDF_Width, PDF_Height]);
 		    pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin,canvas_image_width,canvas_image_height);
-
-
 			// for (var i = 1; i <= totalPDFPages; i++) {
 			// 	pdf.addPage(PDF_Width, PDF_Height);
 			// 	pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height*i)+(top_left_margin*4),canvas_image_width,canvas_image_height);
 			// }
 
-		    pdf.save("HTML-Document.pdf");
-        });
+			// pdf.autoPrint({variant: 'javascript'});
+		    // window.open(pdf.output('bloburl'), '_blank');
+		});
 	}
 
 $( window ).on( "load", function() {
@@ -59,7 +41,10 @@ $( window ).on( "load", function() {
 
     $('.print').click(()=>{
         console.log("print");
-        getPDF();
+        $("#tools").hide();
+		print();
+		$("#tools").show();
+        // getPDF();
     });
 
     $(".A-in").on('change', ()=>{
@@ -105,7 +90,7 @@ $( window ).on( "load", function() {
     	let sum = 0;
 		$(".D-in").each((index, element)=>{
 			if($(element).is(':checked')){
-				sum += 1;
+				if(!$(element).hasClass("zero")){sum += 1;}
 			}
 		});
 		$(".result1-D").text(sum);
